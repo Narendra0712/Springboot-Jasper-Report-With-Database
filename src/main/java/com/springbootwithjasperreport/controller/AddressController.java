f3ed179d1756d9a1954c56c000c442d6dc6ce12a
Narendra0712 <narendrakane45@gmail.com>
2024-07-17 22:16:58 +0530

    New Modifications

--- src/main/java/com/springbootwithjasperreport/controller/AddressController.java
+++ src/main/java/com/springbootwithjasperreport/controller/AddressController.java
@@ -1,23 +1,15 @@
 package com.springbootwithjasperreport.controller;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
-import javax.servlet.http.HttpServletResponse;
-
+import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.springbootwithjasperreport.model.Address;
 import com.springbootwithjasperreport.repository.AddressRepository;
 import com.springbootwithjasperreport.service.JReportService;
-
-import net.sf.jasperreports.engine.JRException;
 
 @RestController
 @CrossOrigin("*")
@@ -35,7 +27,22 @@ public class AddressController {
 	}
 
 	@GetMapping("/jasperpdf/export")
-	public void createPDF(HttpServletResponse response) throws IOException, JRException {
+	public ResponseEntity<Map<String, String>> getReport() {
+        try {
+            byte[] pdfBytes = service.generatePdfReport();
+            String base64Pdf = Base64.getEncoder().encodeToString(pdfBytes);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("pdf", base64Pdf);
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+	
+	/*public void createPDF(HttpServletResponse response) throws IOException, JRException {
 		response.setContentType("application/pdf");
 		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
 		String currentDateTime = dateFormatter.format(new Date());
@@ -45,5 +52,5 @@ public class AddressController {
 		response.setHeader(headerKey, headerValue);
 
 		service.exportJasperReport(response);
-	}
+	}*/
 }
--- src/main/java/com/springbootwithjasperreport/service/JReportService.java
+++ src/main/java/com/springbootwithjasperreport/service/JReportService.java
@@ -1,21 +1,11 @@
 package com.springbootwithjasperreport.service;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
-
 import com.springbootwithjasperreport.model.Address;
 import com.springbootwithjasperreport.repository.AddressRepository;
-
-import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -25,22 +15,41 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class JReportService {
- 
-    @Autowired
-    private AddressRepository repository;
- 
- 
-    public void exportJasperReport(HttpServletResponse response) throws JRException, IOException {
-        List<Address> address = repository.findAll();
-        //Get file and compile it
-        File file = ResourceUtils.getFile("classpath:Address.jrxml");
-        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(address);
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("createdBy", "Narendra");
-        //Fill Jasper report
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-        //Export report
-        JasperExportManager.exportReportToPdfStream(jasperPrint,response.getOutputStream());
-    }
+
+	@Autowired
+	private AddressRepository repository;
+
+	public byte[] generatePdfReport() throws Exception {
+		List<Address> address = repository.findAll();
+		// Compile the Jasper report from .jrxml to .jasper
+		JasperReport jasperReport = JasperCompileManager.compileReport(
+				"D:\\Sprin Boot Projects\\Pdf Generation Using Spring Boot\\Springboot Jasper Report With Database\\src\\main\\resources\\Address.jrxml");
+
+		// Parameters for report
+		Map<String, Object> parameters = new HashMap<>();
+		// Add parameters as needed
+
+		// DataSource
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(address);
+		// Fill the report
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+		// Export the report to a byte array
+		return JasperExportManager.exportReportToPdf(jasperPrint);
+	}
+
+	/*
+	 * public void exportJasperReport(HttpServletResponse response) throws
+	 * JRException, IOException { List<Address> address = repository.findAll();
+	 * //Get file and compile it File file =
+	 * ResourceUtils.getFile("classpath:Address.jrxml"); JasperReport jasperReport =
+	 * JasperCompileManager.compileReport(file.getAbsolutePath());
+	 * JRBeanCollectionDataSource dataSource = new
+	 * JRBeanCollectionDataSource(address); Map<String, Object> parameters = new
+	 * HashMap<>(); parameters.put("createdBy", "Narendra"); //Fill Jasper report
+	 * JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,
+	 * parameters, dataSource); //Export report
+	 * JasperExportManager.exportReportToPdfStream(jasperPrint,response.
+	 * getOutputStream()); }
+	 */
 }
